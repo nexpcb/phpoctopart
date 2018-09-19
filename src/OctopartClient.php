328b0bf3a1daa1271f6fe9e09e479a0a7258d771
Bala Wang <xiaodan.w@outlook.com>
2018-09-19 13:25:03 +0800

    Update api description file and adds apikey option OctopartClient class

--- src/OctopartClient.php
+++ src/OctopartClient.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace Octopart;
+namespace PHPOctopart;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Command\Guzzle\Description;
 use GuzzleHttp\Command\Guzzle\GuzzleClient;
 use Guzzle\Service\Loader\JsonLoader;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\Command\Guzzle\RequestLocation\QueryLocation;
+use GuzzleHttp\Command\Guzzle\QuerySerializer\Rfc3986Serializer;
+use GuzzleHttp\Command\Guzzle\Serializer;
+use Psr\Http\Message\RequestInterface;
 use Symfony\Component\Config\FileLocator;
 
 class OctopartClient extends GuzzleClient
@@ -13,6 +20,10 @@ class OctopartClient extends GuzzleClient
 
     public static function create($config = [])
     {
+        if (!isset($config['apikey'])) {
+            throw new \Exception('Required apikey is missing from configuration.');
+        }
+
         $configDirectories = [__DIR__ . '/Resources'];
         $locator = new FileLocator($configDirectories);
 
@@ -21,13 +32,25 @@ class OctopartClient extends GuzzleClient
         $description = $jsonLoader->load($locator->locate('description.json'));
         $description = new Description($description);
 
+        // Pre-pending apikey to the query parameters.
+        $stack = new HandlerStack();
+        $stack->setHandler(\GuzzleHttp\choose_handler());
+        $stack->unshift(Middleware::mapRequest(function (RequestInterface $request) use ($config) {
+            return $request->withUri(Uri::withQueryValue($request->getUri(), 'apikey', $config['apikey']));
+        }), 'add_apikey');
+
         $client = new Client([
             'headers' => [
                 'Content-Type' => 'application/json',
                 'Accept' => 'application/json',
-            ]
+            ],
+            'handler' => $stack,
         ]);
 
-        return new static($client, $description, NULL, NULL, NULL, $config);
+        // Remove the numeric indices from query params, so it looks like foo[]=bar&foo[]=baz.
+        $queryLocation = new QueryLocation('query', new Rfc3986Serializer(true));
+        $serializer = new Serializer($description, ['query' => $queryLocation]);
+
+        return new static($client, $description, $serializer, NULL, NULL, $config);
     }
 }
